00b9cc6069d8c47269d963610b4469becebec6f1
hnae6443 <hnae6443@gmail.com>
2017-04-22 13:32:53 -0400

    Added code to factory

---
+++ flickrFactory.js
@@ -0,0 +1,7 @@
+angular
+    .module('flickrSearch')
+    .factory('flickrFactory', function($http) {
+        function getPhotos() {
+            return $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=71978388bebe441396682f0f606bb462&tags='+ varSomething + '&format=json&nojsoncallback=1')
+        }
+})
