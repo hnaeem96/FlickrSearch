297c40263629c302fae4a7bc7a00154e01d28b53
hnae6443 <hnae6443@gmail.com>
2017-04-22 14:52:34 -0400

    Modified factory and controller code

--- flickrFactory.js
+++ flickrFactory.js
@@ -1,8 +1,16 @@
 angular
     .module('flickrSearch')
     .factory('flickrFactory', function($http) {
+        
+        var searchTag = '';
+    
+        function setTag(tag) {
+            searchTag = tag;
+            console.log(tag);
+        }
+    
         function getPhotos() {
-            return $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=71978388bebe441396682f0f606bb462&tags='+ varSomething + '&format=json&nojsoncallback=1');
+            return $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=71978388bebe441396682f0f606bb462&tags='+ searchTag + '&format=json&nojsoncallback=1');
         }
     
         return {
