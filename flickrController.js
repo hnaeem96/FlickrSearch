47c3ed820f2d708c2f79d163fa853605ad04a6c0
hnae6443 <hnae6443@gmail.com>
2017-04-23 18:05:37 -0400

    Constructed correct image urls

--- flickrController.js
+++ flickrController.js
@@ -2,17 +2,19 @@ angular
     .module('flickrSearch')
     .controller('flickrController', function($scope, $http) {
     var photos;
-    $scope.images = []
+    $scope.images = [];
     
     $scope.setTag = function(searchTag) {
         var flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=188843a1a5f0b6cd12b9346dc0f9cc81&tags=" + searchTag  + "&format=json&nojsoncallback=1";
         
         $http.get(flickrUrl).success(function(data, status, headers, config) {
-            photos = (data["photos"]["photo"]);
+            $scope.photos = photos = (data["photos"]["photo"]);
             console.log(photos);
             angular.forEach(photos, function(value) {
-                images.push(`https://www.flickr.com/photos/${value['owner']}/${value['id']}`);
+//                $scope.images.push(`https://www.flickr.com/photos/${value['owner']}/${value['id']}`);
+                $scope.images.push(`https://farm${value['farm']}.staticflickr.com/${value['server']}/${value['id']}_${value['secret']}.jpg`);
             });
+            console.log($scope.images);
         })
         .error(function(data, status, headers, config) {
             console.log(data);
