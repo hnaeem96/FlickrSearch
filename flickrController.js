1fc95bee188e6289f8474842657134c98da3269e
hnae6443 <hnae6443@gmail.com>
2017-04-23 13:58:15 -0400

    Sticking with a controller only approach

--- flickrController.js
+++ flickrController.js
@@ -3,13 +3,5 @@ angular
     .controller('flickrController', function($scope, flickrFactory) {
         $scope.searchTag;
         
-        $scope.setTag = function() {
-            flickrFactory.setTag($scope.searchTag);
-        };
         
-        flickrFactory.getPhotos().success(function(data) {
-            console.log(data);
-        }).error(function(error) {
-            console.log(error);
-        });
 });
