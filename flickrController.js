08de429f2677e55acd89438a1f2b41b086a79834
hnae6443 <hnae6443@gmail.com>
2017-04-23 14:47:56 -0400

    Successfully accessed api through controller

--- flickrController.js
+++ flickrController.js
@@ -1,7 +1,15 @@
 angular
     .module('flickrSearch')
-    .controller('flickrController', function($scope, flickrFactory) {
-        $scope.searchTag;
+    .controller('flickrController', function($scope, $http) {
+
+    $scope.setTag = function(searchTag) {
+        var flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=188843a1a5f0b6cd12b9346dc0f9cc81&tags=" + searchTag  + "&format=json&nojsoncallback=1";
         
-        
+        $http.get(flickrUrl).success(function(data, status, headers, config) {
+            console.log(data);
+        })
+        .error(function(data, status, headers, config) {
+            console.log(data);
+        });
+    };
 });
