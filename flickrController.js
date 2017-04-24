6b5f7acae35252e4184dd250299e22d627cd3972
hnae6443 <hnae6443@gmail.com>
2017-04-23 21:15:24 -0400

    Getting ready to implement routing

--- flickrController.js
+++ flickrController.js
@@ -9,7 +9,6 @@ angular
         
         $http.get(flickrUrl).success(function(data, status, headers, config) {
             $scope.photos = photos = (data["photos"]["photo"]);
-            console.log(photos);
             angular.forEach(photos, function(value) {
 //                $scope.images.push(`https://www.flickr.com/photos/${value['owner']}/${value['id']}`);
                 $scope.images.push(`https://farm${value['farm']}.staticflickr.com/${value['server']}/${value['id']}_${value['secret']}.jpg`);
